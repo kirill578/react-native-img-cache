74c20a5f0a68b819208115df911eaabc2f4f4ac9
Kirill Kulakov <Kirill@soluto.com>
2018-01-24 11:14:35 +0200

    es5 build

--- build/index.js
+++ build/index.js
@@ -1,42 +1,52 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 Object.defineProperty(exports, "__esModule", { value: true });
-const react_1 = require("react");
-const react_native_1 = require("react-native");
-const react_native_fetch_blob_1 = require("react-native-fetch-blob");
-const SHA1 = require("crypto-js/sha1");
-const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-const BASE_DIR = react_native_fetch_blob_1.default.fs.dirs.CacheDir + "/react-native-img-cache";
-const FILE_PREFIX = react_native_1.Platform.OS === "ios" ? "" : "file://";
-class ImageCache {
-    constructor() {
+var react_1 = require("react");
+var react_native_1 = require("react-native");
+var react_native_fetch_blob_1 = require("react-native-fetch-blob");
+var SHA1 = require("crypto-js/sha1");
+var s4 = function () { return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1); };
+var BASE_DIR = react_native_fetch_blob_1.default.fs.dirs.CacheDir + "/react-native-img-cache";
+var FILE_PREFIX = react_native_1.Platform.OS === "ios" ? "" : "file://";
+var ImageCache = /** @class */ (function () {
+    function ImageCache() {
         this.cache = {};
     }
-    getPath(uri, immutable) {
-        let path = uri.substring(uri.lastIndexOf("/"));
+    ImageCache.prototype.getPath = function (uri, immutable) {
+        var path = uri.substring(uri.lastIndexOf("/"));
         path = path.indexOf("?") === -1 ? path : path.substring(path.lastIndexOf("."), path.indexOf("?"));
-        const ext = path.indexOf(".") === -1 ? ".jpg" : path.substring(path.indexOf("."));
+        var ext = path.indexOf(".") === -1 ? ".jpg" : path.substring(path.indexOf("."));
         if (immutable === true) {
             return BASE_DIR + "/" + SHA1(uri) + ext;
         }
         else {
             return BASE_DIR + "/" + s4() + s4() + "-" + s4() + "-" + s4() + "-" + s4() + "-" + s4() + s4() + s4() + ext;
         }
-    }
-    static get() {
+    };
+    ImageCache.get = function () {
         if (!ImageCache.instance) {
             ImageCache.instance = new ImageCache();
         }
         return ImageCache.instance;
-    }
-    clear() {
+    };
+    ImageCache.prototype.clear = function () {
         this.cache = {};
         return react_native_fetch_blob_1.default.fs.unlink(BASE_DIR);
-    }
-    on(source, handler, immutable) {
-        const { uri } = source;
+    };
+    ImageCache.prototype.on = function (source, handler, immutable) {
+        var uri = source.uri;
         if (!this.cache[uri]) {
             this.cache[uri] = {
-                source,
+                source: source,
                 downloading: false,
                 handlers: [handler],
                 immutable: immutable === true,
@@ -47,157 +57,178 @@ class ImageCache {
             this.cache[uri].handlers.push(handler);
         }
         this.get(uri);
-    }
-    dispose(uri, handler) {
-        const cache = this.cache[uri];
+    };
+    ImageCache.prototype.dispose = function (uri, handler) {
+        var cache = this.cache[uri];
         if (cache) {
-            cache.handlers.forEach((h, index) => {
+            cache.handlers.forEach(function (h, index) {
                 if (h === handler) {
                     cache.handlers.splice(index, 1);
                 }
             });
         }
-    }
-    bust(uri) {
-        const cache = this.cache[uri];
+    };
+    ImageCache.prototype.bust = function (uri) {
+        var cache = this.cache[uri];
         if (cache !== undefined && !cache.immutable) {
             cache.path = undefined;
             this.get(uri);
         }
-    }
-    cancel(uri) {
-        const cache = this.cache[uri];
+    };
+    ImageCache.prototype.cancel = function (uri) {
+        var cache = this.cache[uri];
         if (cache && cache.downloading) {
             cache.task.cancel();
         }
-    }
-    download(cache) {
-        const { source } = cache;
-        const { uri } = source;
+    };
+    ImageCache.prototype.download = function (cache) {
+        var _this = this;
+        var source = cache.source;
+        var uri = source.uri;
         if (!cache.downloading) {
-            const path = this.getPath(uri, cache.immutable);
+            var path_1 = this.getPath(uri, cache.immutable);
             cache.downloading = true;
-            const method = source.method ? source.method : "GET";
-            cache.task = react_native_fetch_blob_1.default.config({ path }).fetch(method, uri, source.headers);
-            cache.task.then(() => {
+            var method = source.method ? source.method : "GET";
+            cache.task = react_native_fetch_blob_1.default.config({ path: path_1 }).fetch(method, uri, source.headers);
+            cache.task.then(function () {
                 cache.downloading = false;
-                cache.path = path;
-                this.notify(uri);
-            }).catch(() => {
+                cache.path = path_1;
+                _this.notify(uri);
+            }).catch(function () {
                 cache.downloading = false;
                 // Parts of the image may have been downloaded already, (see https://github.com/wkh237/react-native-fetch-blob/issues/331)
-                react_native_fetch_blob_1.default.fs.unlink(path);
+                react_native_fetch_blob_1.default.fs.unlink(path_1);
             });
         }
-    }
-    get(uri) {
-        const cache = this.cache[uri];
+    };
+    ImageCache.prototype.get = function (uri) {
+        var _this = this;
+        var cache = this.cache[uri];
         if (cache.path) {
             // We check here if IOS didn't delete the cache content
-            react_native_fetch_blob_1.default.fs.exists(cache.path).then((exists) => {
+            react_native_fetch_blob_1.default.fs.exists(cache.path).then(function (exists) {
                 if (exists) {
-                    this.notify(uri);
+                    _this.notify(uri);
                 }
                 else {
-                    this.download(cache);
+                    _this.download(cache);
                 }
             });
         }
         else {
             this.download(cache);
         }
-    }
-    notify(uri) {
-        const handlers = this.cache[uri].handlers;
-        handlers.forEach(handler => {
-            handler(this.cache[uri].path);
+    };
+    ImageCache.prototype.notify = function (uri) {
+        var _this = this;
+        var handlers = this.cache[uri].handlers;
+        handlers.forEach(function (handler) {
+            handler(_this.cache[uri].path);
         });
-    }
-}
+    };
+    return ImageCache;
+}());
 exports.ImageCache = ImageCache;
-class BaseCachedImage extends react_1.Component {
-    constructor() {
-        super(...arguments);
-        this.handler = (path) => {
-            this.setState({ path });
+var BaseCachedImage = /** @class */ (function (_super) {
+    __extends(BaseCachedImage, _super);
+    function BaseCachedImage() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.handler = function (path) {
+            _this.setState({ path: path });
         };
+        return _this;
     }
-    dispose() {
+    BaseCachedImage.prototype.dispose = function () {
         if (this.uri) {
             ImageCache.get().dispose(this.uri, this.handler);
         }
-    }
-    observe(source, mutable) {
+    };
+    BaseCachedImage.prototype.observe = function (source, mutable) {
         if (source.uri !== this.uri) {
             this.dispose();
             this.uri = source.uri;
             ImageCache.get().on(source, this.handler, !mutable);
         }
-    }
-    getProps() {
-        const props = {};
-        Object.keys(this.props).forEach(prop => {
-            if (prop === "source" && this.props.source.uri) {
-                props["source"] = this.state.path ? { uri: FILE_PREFIX + this.state.path } : {};
+    };
+    BaseCachedImage.prototype.getProps = function () {
+        var _this = this;
+        var props = {};
+        Object.keys(this.props).forEach(function (prop) {
+            if (prop === "source" && _this.props.source.uri) {
+                props["source"] = _this.state.path ? { uri: FILE_PREFIX + _this.state.path } : {};
             }
             else if (["mutable", "component"].indexOf(prop) === -1) {
-                props[prop] = this.props[prop];
+                props[prop] = _this.props[prop];
             }
         });
         return props;
-    }
-    checkSource(source) {
+    };
+    BaseCachedImage.prototype.checkSource = function (source) {
         if (Array.isArray(source)) {
-            throw new Error(`Giving multiple URIs to CachedImage is not yet supported.
-            If you want to see this feature supported, please file and issue at
-             https://github.com/wcandillon/react-native-img-cache`);
+            throw new Error("Giving multiple URIs to CachedImage is not yet supported.\n            If you want to see this feature supported, please file and issue at\n             https://github.com/wcandillon/react-native-img-cache");
         }
         return source;
-    }
-    componentWillMount() {
-        const { mutable } = this.props;
-        const source = this.checkSource(this.props.source);
+    };
+    BaseCachedImage.prototype.componentWillMount = function () {
+        var mutable = this.props.mutable;
+        var source = this.checkSource(this.props.source);
         this.setState({ path: undefined });
         if (typeof (source) !== "number" && source.uri) {
             this.observe(source, mutable === true);
         }
-    }
-    componentWillReceiveProps(nextProps) {
-        const { mutable } = nextProps;
-        const source = this.checkSource(nextProps.source);
+    };
+    BaseCachedImage.prototype.componentWillReceiveProps = function (nextProps) {
+        var mutable = nextProps.mutable;
+        var source = this.checkSource(nextProps.source);
         if (typeof (source) !== "number" && source.uri) {
             this.observe(source, mutable === true);
         }
-    }
-    componentWillUnmount() {
+    };
+    BaseCachedImage.prototype.componentWillUnmount = function () {
         this.dispose();
-    }
-}
+    };
+    return BaseCachedImage;
+}(react_1.Component));
 exports.BaseCachedImage = BaseCachedImage;
-class CachedImage extends BaseCachedImage {
-    render() {
-        const props = this.getProps();
+var CachedImage = /** @class */ (function (_super) {
+    __extends(CachedImage, _super);
+    function CachedImage() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    CachedImage.prototype.render = function () {
+        var props = this.getProps();
         if (react_1.default.Children.count(this.props.children) > 0) {
             console.warn("Using <CachedImage> with children is deprecated, use <CachedImageBackground> instead.");
         }
-        return react_1.default.createElement(react_native_1.Image, Object.assign({}, props), this.props.children);
-    }
-}
+        return <react_native_1.Image {...props}>{this.props.children}</react_native_1.Image>;
+    };
+    return CachedImage;
+}(BaseCachedImage));
 exports.CachedImage = CachedImage;
-class CachedImageBackground extends BaseCachedImage {
-    render() {
-        const props = this.getProps();
-        return react_1.default.createElement(react_native_1.ImageBackground, Object.assign({}, props), this.props.children);
+var CachedImageBackground = /** @class */ (function (_super) {
+    __extends(CachedImageBackground, _super);
+    function CachedImageBackground() {
+        return _super !== null && _super.apply(this, arguments) || this;
     }
-}
+    CachedImageBackground.prototype.render = function () {
+        var props = this.getProps();
+        return <react_native_1.ImageBackground {...props}>{this.props.children}</react_native_1.ImageBackground>;
+    };
+    return CachedImageBackground;
+}(BaseCachedImage));
 exports.CachedImageBackground = CachedImageBackground;
-class CustomCachedImage extends BaseCachedImage {
-    render() {
-        const { component } = this.props;
-        const props = this.getProps();
-        const Component = component;
-        return react_1.default.createElement(Component, Object.assign({}, props), this.props.children);
+var CustomCachedImage = /** @class */ (function (_super) {
+    __extends(CustomCachedImage, _super);
+    function CustomCachedImage() {
+        return _super !== null && _super.apply(this, arguments) || this;
     }
-}
+    CustomCachedImage.prototype.render = function () {
+        var component = this.props.component;
+        var props = this.getProps();
+        var Component = component;
+        return <Component {...props}>{this.props.children}</Component>;
+    };
+    return CustomCachedImage;
+}(BaseCachedImage));
 exports.CustomCachedImage = CustomCachedImage;
 //# sourceMappingURL=index.js.map
---
+++ src/index.js
@@ -0,0 +1,203 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const react_1 = require("react");
+const react_native_1 = require("react-native");
+const react_native_fetch_blob_1 = require("react-native-fetch-blob");
+const SHA1 = require("crypto-js/sha1");
+const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+const BASE_DIR = react_native_fetch_blob_1.default.fs.dirs.CacheDir + "/react-native-img-cache";
+const FILE_PREFIX = react_native_1.Platform.OS === "ios" ? "" : "file://";
+class ImageCache {
+    constructor() {
+        this.cache = {};
+    }
+    getPath(uri, immutable) {
+        let path = uri.substring(uri.lastIndexOf("/"));
+        path = path.indexOf("?") === -1 ? path : path.substring(path.lastIndexOf("."), path.indexOf("?"));
+        const ext = path.indexOf(".") === -1 ? ".jpg" : path.substring(path.indexOf("."));
+        if (immutable === true) {
+            return BASE_DIR + "/" + SHA1(uri) + ext;
+        }
+        else {
+            return BASE_DIR + "/" + s4() + s4() + "-" + s4() + "-" + s4() + "-" + s4() + "-" + s4() + s4() + s4() + ext;
+        }
+    }
+    static get() {
+        if (!ImageCache.instance) {
+            ImageCache.instance = new ImageCache();
+        }
+        return ImageCache.instance;
+    }
+    clear() {
+        this.cache = {};
+        return react_native_fetch_blob_1.default.fs.unlink(BASE_DIR);
+    }
+    on(source, handler, immutable) {
+        const { uri } = source;
+        if (!this.cache[uri]) {
+            this.cache[uri] = {
+                source,
+                downloading: false,
+                handlers: [handler],
+                immutable: immutable === true,
+                path: immutable === true ? this.getPath(uri, immutable) : undefined
+            };
+        }
+        else {
+            this.cache[uri].handlers.push(handler);
+        }
+        this.get(uri);
+    }
+    dispose(uri, handler) {
+        const cache = this.cache[uri];
+        if (cache) {
+            cache.handlers.forEach((h, index) => {
+                if (h === handler) {
+                    cache.handlers.splice(index, 1);
+                }
+            });
+        }
+    }
+    bust(uri) {
+        const cache = this.cache[uri];
+        if (cache !== undefined && !cache.immutable) {
+            cache.path = undefined;
+            this.get(uri);
+        }
+    }
+    cancel(uri) {
+        const cache = this.cache[uri];
+        if (cache && cache.downloading) {
+            cache.task.cancel();
+        }
+    }
+    download(cache) {
+        const { source } = cache;
+        const { uri } = source;
+        if (!cache.downloading) {
+            const path = this.getPath(uri, cache.immutable);
+            cache.downloading = true;
+            const method = source.method ? source.method : "GET";
+            cache.task = react_native_fetch_blob_1.default.config({ path }).fetch(method, uri, source.headers);
+            cache.task.then(() => {
+                cache.downloading = false;
+                cache.path = path;
+                this.notify(uri);
+            }).catch(() => {
+                cache.downloading = false;
+                // Parts of the image may have been downloaded already, (see https://github.com/wkh237/react-native-fetch-blob/issues/331)
+                react_native_fetch_blob_1.default.fs.unlink(path);
+            });
+        }
+    }
+    get(uri) {
+        const cache = this.cache[uri];
+        if (cache.path) {
+            // We check here if IOS didn't delete the cache content
+            react_native_fetch_blob_1.default.fs.exists(cache.path).then((exists) => {
+                if (exists) {
+                    this.notify(uri);
+                }
+                else {
+                    this.download(cache);
+                }
+            });
+        }
+        else {
+            this.download(cache);
+        }
+    }
+    notify(uri) {
+        const handlers = this.cache[uri].handlers;
+        handlers.forEach(handler => {
+            handler(this.cache[uri].path);
+        });
+    }
+}
+exports.ImageCache = ImageCache;
+class BaseCachedImage extends react_1.Component {
+    constructor() {
+        super(...arguments);
+        this.handler = (path) => {
+            this.setState({ path });
+        };
+    }
+    dispose() {
+        if (this.uri) {
+            ImageCache.get().dispose(this.uri, this.handler);
+        }
+    }
+    observe(source, mutable) {
+        if (source.uri !== this.uri) {
+            this.dispose();
+            this.uri = source.uri;
+            ImageCache.get().on(source, this.handler, !mutable);
+        }
+    }
+    getProps() {
+        const props = {};
+        Object.keys(this.props).forEach(prop => {
+            if (prop === "source" && this.props.source.uri) {
+                props["source"] = this.state.path ? { uri: FILE_PREFIX + this.state.path } : {};
+            }
+            else if (["mutable", "component"].indexOf(prop) === -1) {
+                props[prop] = this.props[prop];
+            }
+        });
+        return props;
+    }
+    checkSource(source) {
+        if (Array.isArray(source)) {
+            throw new Error(`Giving multiple URIs to CachedImage is not yet supported.
+            If you want to see this feature supported, please file and issue at
+             https://github.com/wcandillon/react-native-img-cache`);
+        }
+        return source;
+    }
+    componentWillMount() {
+        const { mutable } = this.props;
+        const source = this.checkSource(this.props.source);
+        this.setState({ path: undefined });
+        if (typeof (source) !== "number" && source.uri) {
+            this.observe(source, mutable === true);
+        }
+    }
+    componentWillReceiveProps(nextProps) {
+        const { mutable } = nextProps;
+        const source = this.checkSource(nextProps.source);
+        if (typeof (source) !== "number" && source.uri) {
+            this.observe(source, mutable === true);
+        }
+    }
+    componentWillUnmount() {
+        this.dispose();
+    }
+}
+exports.BaseCachedImage = BaseCachedImage;
+class CachedImage extends BaseCachedImage {
+    render() {
+        const props = this.getProps();
+        if (react_1.default.Children.count(this.props.children) > 0) {
+            console.warn("Using <CachedImage> with children is deprecated, use <CachedImageBackground> instead.");
+        }
+        return <react_native_1.Image {...props}>{this.props.children}</react_native_1.Image>;
+    }
+}
+exports.CachedImage = CachedImage;
+class CachedImageBackground extends BaseCachedImage {
+    render() {
+        const props = this.getProps();
+        return <react_native_1.ImageBackground {...props}>{this.props.children}</react_native_1.ImageBackground>;
+    }
+}
+exports.CachedImageBackground = CachedImageBackground;
+class CustomCachedImage extends BaseCachedImage {
+    render() {
+        const { component } = this.props;
+        const props = this.getProps();
+        const Component = component;
+        return <Component {...props}>{this.props.children}</Component>;
+    }
+}
+exports.CustomCachedImage = CustomCachedImage;
+//# sourceMappingURL=index.js.map
